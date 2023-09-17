786985389bfa8fd388e90fa0b2eb314ed10457df
Joseph Lee <josephlee3004@gmail.com>
2023-09-17 15:38:08 -0400

    Create search bar and hamburger menu

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+
+hamburgerMenu.addEventListener("click", openSideNav);
+
+function openSideNav() {
+    console.log("open sidenav");
+}
