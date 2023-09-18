c8d6ae502026f3a0d5fe9a8c0640b3e9a2cd5b3a
Joseph Lee <josephlee3004@gmail.com>
2023-09-18 13:21:17 -0400

    Create side nav for smaller screens

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const hamburgerMenu = document.querySelector("#hamburger-menu");
+const sideNav = document.querySelector("#side-nav");
+const closeBtn = document.querySelector("#close-btn");
 
 hamburgerMenu.addEventListener("click", openSideNav);
+closeBtn.addEventListener("click", closeSideNav);
 
 function openSideNav() {
-    console.log("open sidenav");
+    sideNav.style.width = "100%";
+}
+
+function closeSideNav() {
+    sideNav.style.width = "0";
 }
