87839226e92fa71d37246dea3a98088fda5bb4b2
Joseph Lee <josephlee3004@gmail.com>
2023-09-23 11:34:26 -0400

    Disable background scrolling when side nav open

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const hamburgerMenu = document.querySelector("#hamburger-menu");
 const sideNav = document.querySelector("#side-nav");
 const closeBtn = document.querySelector("#close-btn");
+const body = document.querySelector("body");
 
 hamburgerMenu.addEventListener("click", openSideNav);
 closeBtn.addEventListener("click", closeSideNav);
 
 function openSideNav() {
     sideNav.style.width = "100%";
+    body.style.overflow = "hidden"; // Prevent background scrolling
 }
 
 function closeSideNav() {
     sideNav.style.width = "0";
+    body.style.overflow = "auto"; // Re-enable scrolling
 }
